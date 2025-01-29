46f92beb9181068a3da3e5a8762b6ce616972ed0
Andrew <andrew.salygin@gmail.com>
2025-01-29 18:36:40 +0400

    Add files via upload

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,44 @@
-void setup() {
+const int N = 5;
 
+struct PinConfig {
+  volatile uint8_t* port;
+  uint8_t mask;
+};
+
+const PinConfig pins[] = {
+  { &PORTD, (1 << PD3) },
+  { &PORTD, (1 << PD5) },
+  { &PORTD, (1 << PD6) },
+  { &PORTB, (1 << PB1) },
+  { &PORTB, (1 << PB2) }
+};
+
+int delayMcs[] = {10000, 1000, 500, 100, 50};
+int previousMcs[N] = {0};
+int ledStates[N] = {0};
+
+void setup() {
+  for (int i = 0; i < N; i++) {
+    // Data Direction Register
+    volatile uint8_t* ddr = (volatile uint8_t*)(pins[i].port - 1);
+    *ddr |= pins[i].mask;
+  }
 }
 
 void loop() {
+  for (int i = 0; i < N; i++) {
+    unsigned long currentMicros = micros();
+    unsigned long halfPeriod = delayMcs[i] / 2;
 
+    if (currentMicros - previousMcs[i] >= halfPeriod) {
+      if (pinState[i]) {
+        *pins[i].port &= ~pins[i].mask;
+      } else {
+        *pins[i].port |= pins[i].mask;
+      }
+
+      pinState[i] ^= 1;
+      previousMcs[i] = currentMicros;
+    } 
+  }
 }
